831bbdfb714e1befcdeb3609167ffb7b4974e60a
Sudhanshu Kumar <sudhanshu@autopustechnologies.com>
2024-08-29 18:56:50 +0530

    first commit

---
+++ my-learning-projects/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-[400px] w-[400px] flex justify-center items-center'>
+      <h1 className='text-red-500'>Hello brother...</h1>
+      <Button></Button>
+    </div>
+  )
+} 
+
+export default App
